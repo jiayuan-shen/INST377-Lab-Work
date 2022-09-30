579085d9ca4f8174ee6b712161d02cff1e117ed2
jiayuan-shen <shenjiayuan2000@gmail.com>
2022-09-30 12:48:30 -0400

    add the freeze()

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -67,12 +67,24 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
   // make the tetromino move down
-  timerId = setInterval(moveDown, 1000);
+  timerId = setInterval(moveDown, 100);
   // move the tetromino
   function moveDown() {
     undraw();
     currentPosition += width;
     draw();
+    freeze();
 
+  }
+  // freeze the tetromino
+  function freeze() {
+    if (current.some((index) => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach((index) => squares[currentPosition + index].classList.add('taken'));
+      // start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
   }
 });
