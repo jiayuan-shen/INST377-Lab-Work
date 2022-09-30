72e5b7586b733999b2bc89c0ba0e236474be94b7
jiayuan-shen <shenjiayuan2000@gmail.com>
2022-09-30 04:44:48 -0400

    create the templet

---
+++ client/tetris/app.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-loop-func */
+/* eslint-disable no-use-before-define */
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
+})
