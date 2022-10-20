8205706fe7554aa7ec75028c681845e095d478aa
jiayuan-shen <shenjiayuan2000@gmail.com>
2022-10-20 02:42:59 -0400

    all set

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -20,6 +20,17 @@ function getRandomIntInclusive(min, max) {
 
 function injectHTML(list) {
   console.log('fired injectHTML');
+  const target = document.querySelector('#restaurant_list');
+  target.innerHTML = '';
+
+  const listEl = document.createElement('ol');
+  target.appendChild(listEl);
+
+  list.forEach(item =>{
+    const el = document.createElement('li');
+    el.innerText = item.name;
+    listEl.appendChild(el);
+  });
 
   /*
   ## JS and HTML Injection
